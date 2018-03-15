ef58134fcc75b40f5019c552555eb7c40c9a4247
debbie <drosenfeld87@gmail.com>
2018-03-15 10:04:51 -0400

    complete assignment

--- backpack.rb
+++ backpack.rb
@@ -1,6 +1,6 @@
 class Backpack
   def initialize(attributes)
-    @attributes = attributes # a hash containing day_of_week and weather keys
+    @attributes = attributes
     @items = []
     pack_backpack
   end
@@ -10,47 +10,32 @@ class Backpack
   end
 
   def pack_backpack
-    x = @attributes[:weather]
+    weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
 
     def weather
       @attributes[:weather]
-      @items
     end
-    # if x == 'rainy'
-    #   @items << 'pants'
-    #   @items << 'shirt'
-    #   @items << 'umbrella'
-    # elsif x == 'cold'
-    #   @items << 'pants'
-    #   @items << 'shirt'
-    #   @items << 'jacket'
-    # else
-    #   @items << 'pants'
-    #   @items << 'shirt'
-    # end
+    @items = ['pants', 'shirt']
+    if weather == 'rainy'
+      @items << 'umbrella'
+    elsif weather == 'cold'
+      @items << 'jacket'
+    end
 
     def day_of_week
       @attributes[:day_of_week]
       @items
     end
-    # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
     if day_of_week == 'tuesday' || day_of_week == 'thursday'
-      #add gym shoes to items
       @items << 'gym shoes'
     end
 
-    # Bring a packed lunch on all weekdays
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
-    elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
-      @items << 'snacks'
     end
   end
 
-  # Prints a summary packing list for Melinda's backpack
   def items_to_pack
     output = []
     output << "Melinda, here's your packing list!"
